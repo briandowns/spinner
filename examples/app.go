d3d21f271d05cf8aa9a50ab621348721837091ec
Brian Downs <brian.downs@gmail.com>
2014-12-19 20:12:38 -0700

    example update

--- examples/app.go
+++ examples/app.go
@@ -12,13 +12,19 @@ func main() {
 	s.Start()                                                   // Start the spinner
 	time.Sleep(4 * time.Second)                                 // Run for some time to simulate work
 
-	s.UpdateCharSet(spinner.CharSets[1])  // Update spinner to use a different character set
-	s.UpdateSpeed(200 * time.Millisecond) // Update the speed the spinner spins at
+	s.UpdateCharSet(spinner.CharSets[9])  // Update spinner to use a different character set
+	s.UpdateSpeed(100 * time.Millisecond) // Update the speed the spinner spins at
 	s.Restart()                           // Restart the spinner
 	time.Sleep(4 * time.Second)
 
 	s.Reverse() // Reverse the direction the spinner is spinning
 	s.Restart()
+	time.Sleep(4 * time.Second)
+
+	s.Prefix = "prefixed text: " // Prefix text before the spinner
+	time.Sleep(4 * time.Second)
+	s.Prefix = ""
+	s.Suffix = "  :appended text" // Append text after the spinner
 	time.Sleep(4 * time.Second)
 	s.Stop() // Stop the spinner
 }
