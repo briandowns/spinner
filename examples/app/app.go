711663614224b8eeef2b0d936641aed978a7a9e2
Downs <brian.downs@gmail.com>
2015-01-06 11:55:06 -0700

    update example

--- examples/app/app.go
+++ examples/app/app.go
@@ -9,15 +9,18 @@ import (
 
 func main() {
 	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond) // Build our new spinner
+	s.Color("red")                                              // Set the spinner color to red
 	s.Start()                                                   // Start the spinner
 	time.Sleep(4 * time.Second)                                 // Run for some time to simulate work
 
 	s.UpdateCharSet(spinner.CharSets[9])  // Update spinner to use a different character set
 	s.UpdateSpeed(100 * time.Millisecond) // Update the speed the spinner spins at
-	s.Restart()                           // Restart the spinner
+	s.Color("yellow")
+	s.Restart() // Restart the spinner
 	time.Sleep(4 * time.Second)
 
 	s.Reverse() // Reverse the direction the spinner is spinning
+	s.Color("green")
 	s.Restart()
 	time.Sleep(4 * time.Second)
 
