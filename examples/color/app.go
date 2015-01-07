3c22fb745f49a6d9d2189b83a76becc2fbb18c36
io <brian.downs@gmail.com>
2015-01-07 16:24:52 -0700

    Update color example to work the way it should.

--- examples/color/app.go
+++ examples/color/app.go
@@ -15,28 +15,31 @@ func main() {
 	}
 	s.Start()
 	time.Sleep(4 * time.Second)
-
-	s.Color("red")
+	if err := s.Color("red"); err != nil {
+		log.Fatalln(err)
+	}
 	s.UpdateCharSet(spinner.CharSets[20])
 	s.Reverse()
 	s.Restart()
 	time.Sleep(4 * time.Second)
-
-	s.Color("blue")
+	if err := s.Color("blue"); err != nil {
+		log.Fatalln(err)
+	}
 	s.UpdateCharSet(spinner.CharSets[3])
 	s.Restart()
 	time.Sleep(4 * time.Second)
-
-	s.Color("cyan")
+	if err := s.Color("cyan"); err != nil {
+		log.Fatalln(err)
+	}
 	s.UpdateCharSet(spinner.CharSets[28])
 	s.Reverse()
 	s.Restart()
 	time.Sleep(4 * time.Second)
-
-	s.Color("green")
+	if err := s.Color("green"); err != nil {
+		log.Fatalln(err)
+	}
 	s.UpdateCharSet(spinner.CharSets[25])
 	s.Restart()
 	time.Sleep(4 * time.Second)
-
 	s.Stop()
 }
