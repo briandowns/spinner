359940053a98d9692f6291abd9627f260b4ec611
io <brian.downs@gmail.com>
2015-01-07 16:18:20 -0700

    color example

---
+++ examples/color/app.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"time"
+
+	"github.com/briandowns/spinner"
+)
+
+func main() {
+	s := spinner.New(spinner.CharSets[0], 100*time.Millisecond)
+	s.Prefix = "Colors: "
+	s.Color("yellow")
+	s.Start()
+	time.Sleep(4 * time.Second)
+
+	s.Color("red")
+	s.UpdateCharSet(spinner.CharSets[20])
+	s.Reverse()
+	s.Restart()
+	time.Sleep(4 * time.Second)
+
+	s.Color("blue")
+	s.UpdateCharSet(spinner.CharSets[3])
+	s.Restart()
+	time.Sleep(4 * time.Second)
+
+	s.Color("cyan")
+	s.UpdateCharSet(spinner.CharSets[28])
+	s.Reverse()
+	s.Restart()
+	time.Sleep(4 * time.Second)
+
+	s.Color("green")
+	s.UpdateCharSet(spinner.CharSets[25])
+	s.Restart()
+	time.Sleep(4 * time.Second)
+
+	s.Stop()
+}
