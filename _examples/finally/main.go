fe46010ee1d0166b0c84295cec4a19eaa9b917a7
Brian Downs <brian.downs@gmail.com>
2016-02-27 21:53:00 -0700

    fix misnamed field in example code

--- _examples/finally/main.go
+++ _examples/finally/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond) // Build our new spinner
 	s.Color("red")                                              // Set the spinner color to red
-	s.Finally = "Complete!\nNew line!\nAnother one!\n"
+	s.FinalMSG = "Complete!\nNew line!\nAnother one!\n"
 	s.Start()                   // Start the spinner
 	time.Sleep(4 * time.Second) // Run for some time to simulate work
 	s.Stop()                    // Stop the spinner
