cf5aa7a484834c35f80c6e327170b560b58251d1
Bobby Peck <rpeck@brightcove.com>
2017-02-28 18:10:18 -0700

    remove Println

--- spinner_test.go
+++ spinner_test.go
@@ -49,7 +49,6 @@ func TestNew(t *testing.T) {
 	for i := 0; i < len(CharSets); i++ {
 		s := New(CharSets[i], 1*time.Second)
 		if reflect.TypeOf(s).String() != "*spinner.Spinner" {
-			fmt.Println("boom",i)
 			t.Error("New returned incorrect type kind=%d",i)
 		}
 	}
