1cea9f9142187d9101fe41bc3fbef8b422f489ae
Mislav Marohnić <mislav@github.com>
2020-04-10 13:57:45 +0200

    Avoid `erase()` if spinner is not active
    Calling Stop already does an `erase()` and sets `active = false`, so the goroutine should not additionally `erase()` for an inactive spinner.
    
    Additionally, avoid locking the mutex for an inactive spinner because nothing will be written, and previously the mutex remained locked indefinitely.

--- spinner.go
+++ spinner.go
@@ -285,11 +285,11 @@ func (s *Spinner) Start() {
 				case <-s.stopChan:
 					return
 				default:
-					s.mu.Lock()
-					s.erase()
 					if !s.active {
 						return
 					}
+					s.mu.Lock()
+					s.erase()
 
 					if s.PreUpdate != nil {
 						s.PreUpdate(s)
