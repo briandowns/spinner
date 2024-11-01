briandowns/spinner

commit 6e645a1cc3030179b9ffb069f796c213f69ed2e6

add options

spinner.go
@@ -186,8 +186,8 @@ type Spinner struct {
 }
 
 // New provides a pointer to an instance of Spinner with the supplied options
-func New(cs []string, d time.Duration) *Spinner {
-	return &Spinner{
+func New(cs []string, d time.Duration, options ...Option) *Spinner {
+	s:= &Spinner{
 		Delay:    d,
 		chars:    cs,
 		color:    color.New(color.FgWhite).SprintFunc(),
@@ -195,6 +195,38 @@ func New(cs []string, d time.Duration) *Spinner {
 		Writer:   color.Output,
 		active:   false,
 		stopChan: make(chan struct{}, 1),
+	}
+
+	for _, option := range options {
+		option(s)
+	}
+
+	return s
+}
+
+type Option func(*Spinner)
+
+type Options struct {
+	Color string
+	Suffix string
+	FinalMSG string
+}
+
+func WithColor(color string) Option {
+	return func(s *Spinner) {
+		s.Color(color)
+	}
+}
+
+func WithSuffix(suffix string) Option {
+	return func(s *Spinner) {
+		s.Suffix = suffix
+	}
+}
+
+func WithFinalMSG(finalMsg string) Option {
+	return func(s *Spinner) {
+		s.FinalMSG = finalMsg
 	}
 }
 

commit daf221bf2a0d225de4490d527e23c24782d1a29c

Removed unused type 'Options'
Such a helper type for the spinner's options would have been necessary
if the Option functions were invoked before the Spinner object was
instantiated.  However, in this case, the Spinner object is first
instantiated with default values, which are then overwritten by running
the Option functions.

In any case, there's certainly no need for the helper type to be
exported as part of the API.

Fallout from 6e645a1cc3030179b9ffb069f796c213f69ed2e6

spinner.go
@@ -221,14 +221,6 @@ func New(cs []string, d time.Duration, options ...Option) *Spinner {
 // a given configuration.
 type Option func(*Spinner)
 
-// Options contains fields to configure the spinner.
-type Options struct {
-	Color      string
-	Suffix     string
-	FinalMSG   string
-	HideCursor bool
-}
-
 // WithColor adds the given color to the spinner.
 func WithColor(color string) Option {
 	return func(s *Spinner) {